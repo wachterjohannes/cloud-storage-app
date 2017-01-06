b98a8b61988d3eff468d3b61f0244c4fe7e73aee
Johannes Wachter <johannes.wachter@massiveart.com>
2017-01-06 15:34:34 +0100

    removed unused code

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,103 +1,33 @@
-export const REQUEST_STORIES = 'REQUEST_STORIES';
-export const FETCHED_STORIES = 'FETCHED_STORIES';
+export const LOCAL_FILE_LIST_COMPLETED = 'LOCAL_FILE_LIST_COMPLETED';
 
-export const FETCHING_STORY = 'FETCHING_STORY';
-export const FETCHED_STORY = 'FETCHED_STORY';
+export const FILE_ADDED = 'FILE_ADDED';
+export const FILE_UPDATED = 'FILE_UPDATED';
+export const FILE_REMOVED = 'FILE_REMOVED';
 
-export const REQUEST_UPDATE_STORY = 'REQUEST_UPDATE_STORY';
-export const NOTIFY_ABOUT_STORY = 'NOTIFY_ABOUT_STORY';
-export const CLICKED_STORY = 'CLICKED_STORY';
-export const MARK_ALL_AS_READ = 'MARK_ALL_AS_READ';
-export const UPDATE_SCORE_LIMIT = 'UPDATE_SCORE_LIMIT';
-export const ADD_TO_FAVORITES = 'ADD_TO_FAVORITES';
-export const REMOVE_FROM_FAVORITES = 'REMOVE_FROM_FAVORITES';
-export const SWITCH_TAB = 'SWITCH_TAB';
-
-export function requestStories(limit = 50) {
-  return {
-    type: REQUEST_STORIES,
-    limit
-  };
+export function localFileListCompleted(files) {
+    return {
+        type: LOCAL_FILE_LIST_COMPLETED,
+        files
+    }
 }
 
-export function fetchedStories(stories) {
-  return {
-    type: FETCHED_STORIES,
-    stories
-  };
+export function fileAdded(file) {
+    return {
+        type: FILE_ADDED,
+        file
+    };
 }
 
-export function fetchingStory(story) {
-  return {
-    type: FETCHING_STORY,
-    story
-  };
+export function fileUpdated(file) {
+    return {
+        type: FILE_UPDATED,
+        file
+    };
 }
 
-export function fetchedStory(story) {
-  return {
-    type: FETCHED_STORY,
-    story
-  };
-}
-
-export function updateScoreLimit(newScoreLimit) {
-  return {
-    type: UPDATE_SCORE_LIMIT,
-    newScoreLimit
-  }
-}
-
-export function requestUpdateStory(story) {
-  return {
-    type: REQUEST_UPDATE_STORY,
-    story
-  }
-}
-
-export function notifyAboutStory(story, onClick) {
-  const notification = new Notification(`Hacker News ${story.score} 👍💥 votes`, {
-    body: story.title
-  });
-  notification.onclick = onClick;
-
-  return {
-    type: NOTIFY_ABOUT_STORY,
-    story
-  };
-}
-
-export function clickedStory(story) {
-  return {
-    type: CLICKED_STORY,
-    story
-  };
-}
-
-export function markAllAsRead(stories) {
-  return {
-    type: MARK_ALL_AS_READ,
-    stories
-  };
-}
-
-export function addToFavorites(story) {
-  return {
-    type: ADD_TO_FAVORITES,
-    story
-  };
-}
-
-export function removeFromFavorites(story) {
-  return {
-    type: REMOVE_FROM_FAVORITES,
-    story
-  };
-}
-
-export function switchTab(newTab) {
-  return {
-    type: SWITCH_TAB,
-    newTab
-  };
+export function fileRemoved(file) {
+    return {
+        type: FILE_REMOVED,
+        file
+    };
 }
--- src/index.js
+++ src/index.js
@@ -1,32 +1,34 @@
-import 'babel-polyfill'; // generators
+import 'babel-polyfill';
 import React from 'react';
-import { render as renderReact } from 'react-dom';
+import {render as renderReact} from 'react-dom';
 import debounce from 'debounce';
 import configureStore from './store/configureStore';
 
 const state = JSON.parse(localStorage.getItem('state'));
-const store = configureStore(state || {});
+const store = configureStore(state);
 
 let App = require('./components/app').default;
 const render = (Component) => {
-  renderReact(<Component {...store} />, document.getElementById('root'));
+    renderReact(<Component {...store} />, document.getElementById('root'));
 };
 
 if (module.hot) {
-  module.hot.accept('./components/app', function() {
-    let newApp = require('./components/app').default;
-    render(newApp);
-  });
+    module.hot.accept('./components/app', function() {
+        let newApp = require('./components/app').default;
+        render(newApp);
+    });
 }
 
 const saveState = debounce(() => {
-  localStorage.setItem('state', JSON.stringify(store.getState()));
+    localStorage.setItem('state', JSON.stringify(store.getState()));
 }, 1000);
+
 store.subscribe(() => {
-  saveState();
-  render(App);
-  if (process.env.ENV === 'development') {
-    console.log('state', store.getState());
-  }
+    saveState();
+    render(App);
+    if (process.env.ENV === 'development') {
+        console.log('state', store.getState());
+    }
 });
-store.dispatch({ type: 'APP_INIT', store });
+
+store.dispatch({type: 'APP_INIT', store});
